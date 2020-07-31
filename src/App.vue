1fb14d82a7c40dbae285a320f05cdfa5f16f8adc
Rebeka Geczi <geczirebeka@gmail.com>
2020-07-31 11:23:43 +0100

    app and filmlist setup data showing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <template>
-  
+  <div>
+    <h1>Studio Ghibli Films</h1>
+    <div class="main-container">
+      <film-list :films='films'></film-list>
+
+    </div>
+  </div>
 </template>
 
 <script>
+import FilmList from "./components/FilmList.vue"
+
+
 export default {
+  name: 'app',
+  data() {
+    return {
+      films: [],
+      selectedFilm: null
+    };
+  },
+  mounted() {
+    fetch('https://ghibliapi.herokuapp.com/films')
+    .then(response => response.json())
+    .then(films => this.films = films)
+  },
+
+  components: {
+    "film-list": FilmList
+  }
+
+
+
+
 
 }
 </script>
---
+++ src/components/FilmList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <ul>
+          <film-list-item v-for="(film, index) in films" :film="film" :key="index"></film-list-item>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'film-list',
+    props: ['films'],
+
+}
+</script>
+
+<style>
+
+</style>
